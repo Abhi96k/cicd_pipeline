86a9934d5be3dd2ff6e06a73c95cee8479c1e57a
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-25 16:49:36 +0530

    Update default GET route in index.js to redirect to an image URL

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ import express from "express";
 
 const app = express();
 
+// Route to handle the default GET request
 app.get("/", (req, res) => {
-  return res.json({
-    status: "200",
-    message: "Hello World  How are You i woprking on docker   f",
-    message:
-      "Hello World  How are You i woprking on docker cx Hiii  ii   kesa hi bhaiii hiiii",
-  });
+  res.redirect(
+    "https://static.tnn.in/thumb/msid-99167833,thumbsize-81038,width-1280,height-720,resizemode-75/99167833.jpg?quality=100"
+  );
 });
 
+// Start the server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
   console.log("http://localhost:3000");
